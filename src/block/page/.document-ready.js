a079d3911c7044e93b68a079039a9f5fd89dc960
caezar47 <iss47@ya.ru>
2020-04-27 12:16:54 +0300

    Добавил страницу Карточка сделки Платежи

--- src/block/page/.document-ready.js
+++ src/block/page/.document-ready.js
@@ -1,10 +1,16 @@
 $('[data-toggle="tooltip"]').tooltip();
+
+if ($(window).width() < 1025) {
+	$('[data-toggle="tooltip"]').tooltip({  
+		trigger: 'click'
+	});
+}
 /*var result = $("[data-count]"),
 	carousel = $('[data-carousel-count]'),
-    count = $('.item', carousel).length,
-    slideCarousel = function () {
-        var index = $('.item.active', carousel).index()+1;
-        result.html("<span>"+index+"</span><span>/</span><span>"+count+"</span>");
+	count = $('.item', carousel).length,
+	slideCarousel = function () {
+		var index = $('.item.active', carousel).index()+1;
+		result.html("<span>"+index+"</span><span>/</span><span>"+count+"</span>");
 	};
 carousel.on('slid.bs.carousel', slideCarousel).trigger('slid.bs.carousel');
 
@@ -15,9 +21,9 @@ btn.on('click', function(e) {
 });
 $('#articles').on('hidden.bs.collapse',function(e){
 	if($(e.target).hasClass('panel-collapse')){
-        $(e.target).collapse('show');
-        //e.preventDefault();
+		$(e.target).collapse('show');
+		//e.preventDefault();
 		//e.stopPropagation();
-  	}
+	}
 });
 */
